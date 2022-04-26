c09e62bb7420d47bed65e334f446cc2d6d20941a
Sean <sean5446@gmail.com>
2022-04-26 10:24:14 -0400

    Create simple-scrape.pl

---
+++ simple-scrape.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+my @symbols = qw/
+
+AAPL
+BEN
+/;
+my $user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36";
+my $dir = 'quotes';
+
+`mkdir -p $dir`;
+
+my $i = 0;
+my $count = scalar(@symbols);
+for my $sym (@symbols) {
+    $i++;
+    print("$sym $i/$count\n");
+    `wget -P "$dir" -U "$user_agent" "https://finance.yahoo.com/quote/$sym"`;
+}
+

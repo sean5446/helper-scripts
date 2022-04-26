27fecd4bd9fb3d6a966fa1635c3f6efef2ee8164
Sean <sean5446@gmail.com>
2022-04-26 10:25:07 -0400

    Update simple-scrape.pl

--- simple-scrape.pl
+++ simple-scrape.pl
@@ -4,13 +4,12 @@ use strict;
 use warnings;
 
 my @symbols = qw/
-
 AAPL
 BEN
 /;
+
 my $user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36";
 my $dir = 'quotes';
-
 `mkdir -p $dir`;
 
 my $i = 0;

a4161bb539183c58e7a117ba109397dbc132f098
Sean <sean5446@gmail.com>
2024-01-26 10:33:42 -0500

    Update and rename remsnap.sh to remove-old-snaps.sh

--- remove-old-snaps.sh
+++ remove-old-snaps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#Removes old revisions of snaps
-#CLOSE ALL SNAPS BEFORE RUNNING THIS
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
 set -eu
 LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
